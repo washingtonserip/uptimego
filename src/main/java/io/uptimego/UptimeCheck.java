e7478d929b3d6e867b5acc4c644262daed72c061
Washington <22279738+washingtonbr@users.noreply.github.com>
2023-08-02 21:40:53 -0300

    feat: add logs

--- src/main/java/io/uptimego/UptimeCheck.java
+++ src/main/java/io/uptimego/UptimeCheck.java
@@ -1,11 +1,14 @@
 package io.uptimego;
 
 import io.uptimego.domain.Target;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 public class UptimeCheck {
+    private static final Logger logger = LoggerFactory.getLogger(UptimeCheck.class);
 
     private final WebClient webClient;
 
@@ -19,9 +22,11 @@ public class UptimeCheck {
                 .exchangeToMono(response -> {
                     HttpStatus status = response.statusCode();
                     if (status.is2xxSuccessful()) {
+                        logger.info("Target {} is up", target.getDomain());
                         return response.bodyToMono(String.class)
                                 .map(body -> new CheckResult(target.getId(), status.value(), body));
                     } else {
+                        logger.warn("Target {} is down", target.getDomain());
                         return Mono.just(new CheckResult(target.getId(), status.value(), "Service is down"));
                     }
                 });
