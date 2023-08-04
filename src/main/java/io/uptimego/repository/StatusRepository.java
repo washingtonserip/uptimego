fc06850823b210a5c3c18370085c6f25992b32f3
Washington <22279738+washingtonbr@users.noreply.github.com>
2023-08-04 08:52:07 -0300

    chore: reset the project

--- src/main/java/io/uptimego/repository/StatusRepository.java
+++
@@ -1,8 +0,0 @@
-package io.uptimego.repository;
-
-import io.uptimego.domain.Status;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatusRepository extends JpaRepository<Status, Integer> {
-    // Métodos CRUD e de manipulação de banco de dados são gerados automaticamente
-}
--- src/main/java/io/uptimego/repository/TargetRepository.java
+++
@@ -1,8 +0,0 @@
-package io.uptimego.repository;
-
-import io.uptimego.domain.Target;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TargetRepository extends JpaRepository<Target, Integer> {
-    // Métodos CRUD e de manipulação de banco de dados são gerados automaticamente
-}
