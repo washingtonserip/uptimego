369dc5c3d1a3636a2d46a923b84887aee0e4d82a
Washington <22279738+washingtonbr@users.noreply.github.com>
2023-08-20 16:03:16 -0300

    fix: fix target controller test

--- src/test/java/io/uptimego/controller/TargetControllerTest.java
+++ src/test/java/io/uptimego/controller/TargetControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -29,6 +30,9 @@ public class TargetControllerTest {
 
     @MockBean
     private TargetService service;
+
+    @MockBean
+    private JavaMailSender javaMailSender;
 
     @Test
     @WithMockUser(username = "testUser", roles = {"USER"})
