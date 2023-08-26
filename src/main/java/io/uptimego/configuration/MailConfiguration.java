fdd3729cae81b505b5df25023b283f3be808aed0
Washington <22279738+washingtonbr@users.noreply.github.com>
2023-08-26 08:52:06 -0300

    feat: add mail configuration

---
+++ src/main/java/io/uptimego/configuration/MailConfiguration.java
@@ -0,0 +1,30 @@
+package io.uptimego.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfiguration {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.example.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("your.email@example.com");
+        mailSender.setPassword("your-password");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
