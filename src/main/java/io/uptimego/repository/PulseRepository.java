abceef6473bd0ef6dc00f926a413b49e72563697
Washington <22279738+washingtonbr@users.noreply.github.com>
2023-09-30 19:23:55 -0300

    SBM: applied recipe 'sbu30-paging-and-sorting-repository'

--- src/main/java/io/uptimego/repository/PulseRepository.java
+++ src/main/java/io/uptimego/repository/PulseRepository.java
@@ -6,13 +6,14 @@ import io.uptimego.model.Target;
 import io.uptimego.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PulseRepository extends PagingAndSortingRepository<Pulse, Long> {
+public interface PulseRepository extends PagingAndSortingRepository<Pulse, Long>, CrudRepository<Pulse, Long> {
     Page<Pulse> findByStatus(PulseStatus pulseStatus, Pageable pageable);
 
     List<Pulse> findByUser(User user);
